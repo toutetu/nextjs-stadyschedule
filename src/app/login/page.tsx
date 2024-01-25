354d1bc41920ec62d2cb215ec28c86a70c2360d6
tomoko takahashi <toutetu144@gmail.com>
2024-01-25 15:14:23 +0900

    ログインフォーム変更

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
         ログインフォームを表示する予定です。
         
+        <div className='flex '>メールアドレス &nbsp;&nbsp;  <input type="text" className='border-2 w-100 text-right'/> </div>
+        <div className='flex '>パスワード&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" className='border-2 w-100 text-right'/> </div>
+
         <Link
           href="../"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
